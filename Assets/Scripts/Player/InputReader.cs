d4fc664fd914be14eb2725e618bb94463764b020
Orc1d3a <ZevsTK@gmail.com>
2026-04-05 19:43:20 +0300

    adding vampirism

--- Assets/Scripts/Player/InputReader.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-public static class InputReader
-{
-    public static float HorizontalAxis => Input.GetAxisRaw("Horizontal");
-    public static bool IsJumpPressed => Input.GetButtonDown("Jump");
-}
